4d64446324aaa66a387833e207305cefb109d218
Dhishan Chathuranga <dhishanc.creation@gmail.com>
2023-08-25 01:14:43 +0530

    developed Mobile+ Header

--- ecommerce-web-app/src/App.jsx
+++ ecommerce-web-app/src/App.jsx
@@ -4,11 +4,11 @@ import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <>
+    <div className=" w-full h-screen ">
       <Header />
       <Body />
       <Footer />
-    </>
+    </div>
   );
 }
 
--- ecommerce-web-app/src/Components/Header/Header.jsx
+++ ecommerce-web-app/src/Components/Header/Header.jsx
@@ -1,8 +1,41 @@
+import menu from "../../img/hamburger-menu-svgrepo-com.svg";
+import search from "../../img/search-alt-svgrepo-com.svg";
+import cart from "../../img/cart-large-svgrepo-com.svg";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className=" fixed z-[100] w-full p-5 bg-my-bg flex items-center justify-between drop-shadow-header-shadow">
+      {/* Header Left */}
+      <div className=" flex items-center ">
+        <img
+          src={menu}
+          alt="Menu"
+          className=" w-6 h-6 object-contain cursor-pointer"
+        />
+        <h1 className=" text-base font-bold ml-1">
+          DC <span className="  text-purple-950">Creation</span>
+        </h1>
+        <div className=" flex ml-3 items-center bg-my-bg shadow-md pr-2 rounded-full">
+          <input
+            type="text"
+            placeholder="Search"
+            className=" p-1 pl-3 bor rounded-2xl text-sm shadow-md"
+          />
+          <img
+            src={search}
+            alt="Search"
+            className=" w-6 h-6 object-contain cursor-pointer ml-1"
+          />
+        </div>
+      </div>
+      {/* Header Right */}
+      <img
+        src={cart}
+        alt="Cart"
+        className=" w-6 h-6 object-contain cursor-pointe"
+      />
+    </header>
+  );
+};
 
 export default Header;
