6f7475ebaf1685363fb0662014b83b9a967244e0
Dhishan Chathuranga <dhishanc.creation@gmail.com>
2023-08-24 20:47:18 +0530

    Setup Main Components and folder structure

--- ecommerce-web-app/src/App.jsx
+++ ecommerce-web-app/src/App.jsx
@@ -1,6 +1,14 @@
+import Body from "./Components/Body/Body";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
   );
 }
 
