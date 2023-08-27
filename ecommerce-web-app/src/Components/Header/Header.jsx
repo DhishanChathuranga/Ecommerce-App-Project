d13d700d7fa2e0fa0a9e1f5cc83d386c118e0daa
Dhishan Chathuranga <dhishanc.creation@gmail.com>
2023-08-27 16:20:55 +0530

    Use meteriel-ui

--- ecommerce-web-app/src/Components/Header/Header.jsx
+++ ecommerce-web-app/src/Components/Header/Header.jsx
@@ -1,17 +1,13 @@
-import menu from "../../img/hamburger-menu-svgrepo-com.svg";
-import search from "../../img/search-alt-svgrepo-com.svg";
-import cart from "../../img/cart-large-svgrepo-com.svg";
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Header = () => {
   return (
     <header className=" fixed z-[100] w-full p-5 bg-my-bg flex items-center justify-between drop-shadow-header-shadow">
       {/* Header Left */}
       <div className=" flex items-center ">
-        <img
-          src={menu}
-          alt="Menu"
-          className=" w-6 h-6 object-contain cursor-pointer"
-        />
+        <MenuIcon />
         <h1 className=" text-base font-bold ml-1">
           DC <span className="  text-purple-950">Creation</span>
         </h1>
@@ -21,19 +17,11 @@ const Header = () => {
             placeholder="Search"
             className=" p-1 pl-3 bor rounded-2xl text-sm shadow-md"
           />
-          <img
-            src={search}
-            alt="Search"
-            className=" w-6 h-6 object-contain cursor-pointer ml-1"
-          />
+          <SearchIcon className=" ml-1"/>
         </div>
       </div>
       {/* Header Right */}
-      <img
-        src={cart}
-        alt="Cart"
-        className=" w-6 h-6 object-contain cursor-pointe"
-      />
+      <ShoppingCartIcon />
     </header>
   );
 };
